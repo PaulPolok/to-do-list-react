194ba5a423d00237b4b478e006ca3e0f1983f8bf
Paweł Polok <paw.polok@gmail.com>
2022-08-30 19:26:10 +0200

    newTaskAdd interaction

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Header from './Header';
 function App() {
 
   const [hideDone, setHideDone] = useState(false);
+
   const [tasks, setTasks] = useState(
     [
       { id: 1, content: 'Zjeść kebsa', done: false },
@@ -17,13 +18,15 @@ function App() {
     ]
   );
 
+
+
   const toggleHideDone = () => {
     setHideDone(hideDone => !hideDone);
   };
 
   const removeTask = (id) => {
     setTasks(tasks => tasks.filter(task => task.id !== id))
-  }
+  };
 
   const toggleTaskDone = (id) => {
     setTasks(tasks => tasks.map(task => {
@@ -32,13 +35,25 @@ function App() {
       }
       return task;
     }));
-  }
+  };
 
   const setAllDone = () => {
     setTasks(tasks => tasks.map(
       task => ({ ...task, done: true })
     ))
-  }
+  };
+
+  const addNewTask = (content) => {
+    setTasks(tasks => [
+      ...tasks,
+      {
+        content,
+        done: false,
+        id: tasks.length === 0 ? 1 : tasks[tasks.length - 1].id + 1,
+      },
+    ]);
+  };
+
   return (
     <div>
       <Container>
@@ -48,7 +63,7 @@ function App() {
 
         <Section
           title='Dodaj nowe zadanie'
-          body={<Form />}
+          body={<Form addNewTask={addNewTask} />}
         />
 
         <Section
