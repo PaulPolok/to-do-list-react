6cbb063c2614636467c2ad5c4c31c9542ba998bd
Paweł Polok <paw.polok@gmail.com>
2022-09-04 14:16:13 +0200

    Task List cleaned

--- src/App.js
+++ src/App.js
@@ -11,12 +11,7 @@ function App() {
 
   const [hideDone, setHideDone] = useState(false);
 
-  const [tasks, setTasks] = useState(
-    [
-      { id: 1, content: 'Zjeść kebsa', done: false },
-      { id: 2, content: 'Zrobić stronę', done: true }
-    ]
-  );
+  const [tasks, setTasks] = useState([]);
 
 
 
